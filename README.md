a9e546d6718ff05bfdfcd05281817802b7c9f11c
James Hart <jhart@emiliohealth.com>
2020-07-28 12:51:32 -0700

    Some pretty good progress!

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 HEADLESS=false bundle exec kimurai console --engine selenium_chrome --url "https://washington.goingtocamp.com/create-booking/results?mapId=-2147483346&searchTabGroupId=0&bookingCategoryId=0&startDate=2020-08-01T00:00:00.000Z&endDate=2020-08-02T00:00:00.000Z&nights=1&isReserving=true&equipmentId=-32768&subEquipmentId=-32768&partySize=2&searchTime=Mon%20Jul%2027%202020%2017:36:28%20GMT-0700%20(Pacific%20Daylight%20Time)&resourceLocationId=-2147483538"
+
+HEADLESS=false ruby src/campground.rb 
+
+ruby src/campground.rb 
+
+cat results.json | jq 'map(.ada_only)'
--- src/campground.rb
+++ src/campground.rb
@@ -6,22 +6,25 @@ class Campground < Kimurai::Base
 
   @name = "campground"
   @engine = :selenium_chrome
-  @start_urls = ["https://washington.goingtocamp.com/create-booking/results?mapId=-2147483346&searchTabGroupId=0&bookingCategoryId=0&startDate=2020-08-01T00:00:00.000Z&endDate=2020-08-02T00:00:00.000Z&nights=1&isReserving=true&equipmentId=-32768&subEquipmentId=-32768&partySize=2&searchTime=Mon%20Jul%2027%202020%2017:36:28%20GMT-0700%20(Pacific%20Daylight%20Time)&resourceLocationId=-2147483538"]
-
-  def wait_until_breadcrumb_updated_to_display(name)
-    # Wait until the deepest breadcrumb equals name
-    browser.find(:css, "ol[aria-label='Search Result Breadcrumbs'] li button[disabled]", text: name, match: :first)
-  end
-
-  def show_and_hide_available_locations()
-    unavailable_items = browser.all(:css, '.resource-availability', text: /Unavailable|Non-Reservable/)
-    while unavailable_items.present?
-      checkbox = browser.find(:css, '.mat-checkbox-label', text: /Show available .* only/, match: :first)
-      checkbox.click
-      sleep 0.5
-      unavailable_items = browser.all(:css, '.resource-availability', text: /Unavailable|Non-Reservable/, wait: 0.5)
-    end
-  end
+  start_date = "2020-08-01T00:00:00.000Z"
+  end_date = "2020-08-02T00:00:00.000Z"
+  party_size = 2
+  params = { 
+    "mapId" => -2147483346,
+    "searchTabGroupId" => 0,
+    "bookingCategoryId" => 0,
+    "startDate" => start_date,
+    "endDate" => end_date,
+    "nights" => 1,
+    "isReserving" => true,
+    "equipmentId" => -32768,
+    "subEquipmentId" => -32768,
+    "partySize" => party_size,
+    "searchTime" => "Mon%20Jul%2027%202020%2017:36:28%20GMT-0700%20(Pacific%20Daylight%20Time)",
+    "resourceLocationId" => "-2147483538"
+  }
+  start_url = URI::HTTPS.build(host: "washington.goingtocamp.com", path: "/create-booking/results", query: params.to_query)
+  @start_urls = [start_url.to_s]
 
   def parse(response, url:, data: {})
     browser.click_on 'I Consent'
@@ -33,56 +36,92 @@ class Campground < Kimurai::Base
     visited_plot_names = []
 
     browser.click_on 'LIST View'
-    while browser.all(:css, ".resource-name div", text: none_of_these_words_regexp(visited_campground_names)).present?
+    while browser.all(:css, ".resource-name div", text: none_of_these_words_regexp(visited_campground_names), wait: 2).present?
       # Click on campground
       campground_selection_url = browser.current_url
       campground_element = browser.all(:css, ".resource-name div", text: none_of_these_words_regexp(visited_campground_names)).first
+      logger.info "Found element with html #{campground_element['innerHTML']}"
       campground_name = campground_element.text
+      break if campground_name.blank?
       visited_campground_names << campground_name
       campground_element.click
 
       wait_until_breadcrumb_updated_to_display(campground_name)
       show_and_hide_available_locations
 
-      # Click on campground section
-      section_element = browser.all(:css, ".resource-name div", text: none_of_these_words_regexp(visited_section_names)).first
-      section_name = section_element.text
-      visited_section_names << section_name
-      section_element.click
+      while browser.all(:css, ".resource-name div", text: none_of_these_words_regexp(visited_section_names), wait: 2).present?
+        # Click on campground section
+        section_selection_url = browser.current_url
+        section_element = browser.all(:css, ".resource-name div", text: none_of_these_words_regexp(visited_section_names)).first
+        logger.info "Found element with html #{section_element['innerHTML']}"
+        section_name = section_element.text
+        break if section_name.blank?
+        visited_section_names << section_name
+        section_element.click
 
-      wait_until_breadcrumb_updated_to_display(section_name)
-      show_and_hide_available_locations
+        wait_until_breadcrumb_updated_to_display(section_name)
+        show_and_hide_available_locations
 
-      # Click on campground plot for details
-      plot_element = browser.all(:css, ".resource-name div", text: none_of_these_words_regexp(visited_plot_names)).first
-      plot_name = plot_element.text
-      visited_plot_names << plot_name
-      plot_element.click
-      
-      wait_until_breadcrumb_updated_to_display(section_name)
-      show_and_hide_available_locations
+        while browser.all(:css, ".resource-name div", text: none_of_these_words_regexp(visited_plot_names), wait: 2).present?
+          # Click on campground plot for details
+          plot_selection_url = browser.current_url
+          plot_element = browser.all(:css, ".resource-name div", text: none_of_these_words_regexp(visited_plot_names)).first
+          logger.info "Found element with html #{plot_element['innerHTML']}"
+          plot_name = plot_element.text
+          break if plot_name.blank?
+          hide_covid_notice
+          begin
+            plot_element.click
+          rescue Selenium::WebDriver::Error::ElementNotInteractableError => e
+            logger.error e.message
+            break
+          end
+          visited_plot_names << plot_name
 
-      # Gather details
-      text = browser.find(:css, '.view-details', match: :first).text
-      
-      image = browser.find(:css, '.site-image', match: :first)['src']
-      i = 0
-      details = {}
-      more_details = browser.all(:css, '.more-details div div')
-      while (i < more_details.size) do
-        value = more_details[i].text 
-        key = more_details[i+1].text
-        value.gsub!(key, "")
-        details[key] = value
-        i+=2
+          # Gather details
+          logger.debug "Grabbing details"
+          text = browser.find(:css, '.view-details', match: :first).text
+          
+          logger.debug "Grabbing image"
+          image = browser.find(:css, '.site-image', match: :first)['src']
+          i = 0
+          details = {}
+          logger.debug "Grabbing more details..."
+          more_details = browser.all(:css, '.more-details div div')
+          while (i < more_details.size) do
+            value = more_details[i].text 
+            key = more_details[i+1].text.underscore
+            value.gsub!(key, "")
+            details[key] = value.strip
+            i+=2
+          end
+          directions_url = URI::HTTPS.build(host: "maps.google.com", path: "/maps", query: {saddr: "1911 18th ave s 98144", daddr: campground_name}.to_query)
+          item_name = "#{campground_name} > #{section_name} > #{plot_name}"
+          item = { 
+            campground_name: item_name, 
+            directions_url: directions_url,
+            campground_url: section_selection_url,
+            booking_url: plot_selection_url, 
+            description: text, 
+            image: image 
+
+          }.merge(details)
+          items[campground_name] = item
+          # End adding unique item
+          logger.debug "Saving item #{item_name}..."
+          save_to "results.json", item, format: :pretty_json
+
+          browser.visit plot_selection_url
+          browser.click_on 'LIST View'
+        end
+        visited_plot_names = []
+        browser.visit section_selection_url
+        browser.click_on 'LIST View'
       end
-      item = { campground_name: "#{campground_name} > #{section_name} > #{plot_name}", description: text, image: image }.merge(details)
-      items[campground_name] = item
-      logger.info "Visiting #{campground_selection_url}"
+      visited_section_names = []
       browser.visit campground_selection_url
       browser.click_on 'LIST View'
     end
-    save_to "results.json", items, format: :pretty_json
   end
 
   private
@@ -91,6 +130,41 @@ class Campground < Kimurai::Base
     return nil if visited_names.empty?
     logger.debug "Filtering out #{visited_names.join(", ")}"
     Regexp.new("^(?!.*(#{visited_names.join('|')})).*$")
+  end
+
+  def wait_until_breadcrumb_updated_to_display(name)
+    # Wait until the deepest breadcrumb equals name
+    browser.find(:css, "ol[aria-label='Search Result Breadcrumbs'] li button[disabled]", text: name, match: :first)
+  end
+
+  def show_and_hide_available_locations()
+    unavailable_items = browser.all(:css, '.resource-availability', text: /Unavailable|Non-Reservable/, wait: 0.5)
+    while unavailable_items.present?
+      checkbox = browser.find(:css, '.mat-checkbox-label', text: /Show available .* only/, match: :first)
+      checkbox.click
+      sleep 0.5
+      unavailable_items = browser.all(:css, '.resource-availability', text: /Unavailable|Non-Reservable/, wait: 0.5)
+    end
+  end
+
+  def hide_covid_notice()
+    covid_notice = browser.all(:css, '.mat-checkbox-label', text: "I have read and acknowledge all of the park alerts listed.", wait: 0.5)
+    if covid_notice.present?
+      checkbox = browser.find(:css, '.mat-checkbox-label', text: "I have read and acknowledge all of the park alerts listed.")
+      checkbox.click
+    end
+  end
+
+end
+
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    tr(" ", "_").
+    downcase
   end
 end
 
