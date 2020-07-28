ab6bda83f976709f473adca2776c97c3edd35710
James Hart <jhart@emiliohealth.com>
2020-07-28 10:33:46 -0700

    More resiliency

--- src/campground.rb
+++ src/campground.rb
@@ -6,6 +6,21 @@ class Campground < Kimurai::Base
   @engine = :selenium_chrome
   @start_urls = ["https://washington.goingtocamp.com/create-booking/results?mapId=-2147483346&searchTabGroupId=0&bookingCategoryId=0&startDate=2020-08-01T00:00:00.000Z&endDate=2020-08-02T00:00:00.000Z&nights=1&isReserving=true&equipmentId=-32768&subEquipmentId=-32768&partySize=2&searchTime=Mon%20Jul%2027%202020%2017:36:28%20GMT-0700%20(Pacific%20Daylight%20Time)&resourceLocationId=-2147483538"]
 
+  def wait_until_breadcrumb_updated_to_display(name)
+    # Wait until the deepest breadcrumb equals name
+    browser.find(:css, "ol[aria-label='Search Result Breadcrumbs'] li button[disabled]", text: name, match: :first)
+  end
+
+  def show_and_hide_available_locations()
+    unavailable_items = browser.all(:css, '.resource-availability', text: /Unavailable|Non-Reservable/)
+    while unavailable_items.present?
+      checkbox = browser.find(:css, '.mat-checkbox-label', text: /Show available .* only/, match: :first)
+      checkbox.click
+      sleep 0.5
+      unavailable_items = browser.all(:css, '.resource-availability', text: /Unavailable|Non-Reservable/, wait: 0.5)
+    end
+  end
+
   def parse(response, url:, data: {})
     browser.click_on 'I Consent'
     response = browser.current_response
@@ -15,18 +30,23 @@ class Campground < Kimurai::Base
     #   campground.click
     # end
 
-    # require 'pry'
-    # binding.pry
     browser.click_on 'LIST View'
     # browser.find(:css, 'div#map .available', match: :first).click
 
-    name = browser.find(:css, '.resource-name div', match: :first).text
+    campground_name = browser.find(:css, '.resource-name div', match: :first).text
+    # TODO: Grab number of campgrounds below
     browser.find(:css, "[aria-controls=site-details] .icon-available", match: :first).click
 
+    wait_until_breadcrumb_updated_to_display(campground_name)
+    show_and_hide_available_locations
     # Click on campground section
+    section_name = browser.find(:css, '.resource-name div', match: :first).text
     browser.find(:css, "[aria-controls=site-details] .icon-available", match: :first).click
-  
+    wait_until_breadcrumb_updated_to_display(section_name)
+    show_and_hide_available_locations
+
     # Click on campground plot for details
+    plot_name = browser.find(:css, '.resource-name div', match: :first).text
     browser.find(:css, "[aria-controls=site-details] .icon-available", match: :first).click
     
     # Gather details
@@ -43,7 +63,7 @@ class Campground < Kimurai::Base
       details[key] = value
       i+=2
     end
-    item = { name: name, description: text, image: image }.merge(details)
+    item = { campground_name: "#{campground_name} > #{section_name} > #{plot_name}", description: text, image: image }.merge(details)
 
     save_to "results.json", item, format: :pretty_json
   end
