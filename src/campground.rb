61566d65107b03e00e5cc1321b72426722e6f2e6
James Hart <jhart@emiliohealth.com>
2020-08-17 11:30:48 -0700

    tweaks

--- src/campground.rb
+++ src/campground.rb
@@ -11,9 +11,9 @@ class Campground < Kimurai::Base
 
   @name = "campground"
   @engine = :selenium_chrome
-  @start_date = "2020-08-28T00:00:00.000Z"
+  @start_date = "2020-08-29T00:00:00.000Z"
   @end_date = "2020-08-30T00:00:00.000Z"
-  party_size = 4
+  party_size = 2
 
   params = { 
     "mapId" => -2147483346,
@@ -23,10 +23,9 @@ class Campground < Kimurai::Base
     "endDate" => @end_date,
     "isReserving" => true,
     "equipmentId" => -32768,
-    # "subEquipmentId" => -32768, # 1 tent
-    "subEquipmentId" => -32767, # 2 tent
+    "subEquipmentId" => -32768, # 1 tent
+    # "subEquipmentId" => -32767, # 2 tent
     "partySize" => party_size,
-    # "searchTime" => "Mon Jul 27 2020 17:36:28 GMT-0700 (Pacific Daylight Time)",
     "resourceLocationId" => "-2147483538"
   }
   start_url = URI::HTTPS.build(host: "washington.goingtocamp.com", path: "/create-booking/results", query: params.to_query)
@@ -38,19 +37,27 @@ class Campground < Kimurai::Base
     browser.click_on 'I Consent'
     response = browser.current_response
 
-    filtered_campground_names = ["Camano Island​", "Kitsap Memorial​", "Larrabee​", "South Whidbey​", "Twanoh", "Deception Pass", "Retreat Center - Cornet Bay​", "Potlatch​", "Rasar​", "Fort Ebey​", "Schafer", "Rockport​", "Birch Bay", "Fort Townsend", "Lake Wenatchee​", "Peace Arch", "Sequim Bay​", "Retreat Center - Ramblewood​", "Fort Casey​", "Fort Worden​", "Dosewallups", "Fort Flagler​", "Ocean City​", "Retreat Center - Fort Flagler​", "Jarrel Cove", "Pacific Beach​", "Spencer Spit​", "Griffiths-Priday​", "Jones Island​", "Moran​", "Retreat Center - Camp Moran​", "Bogachiel", "Sucia Island​", "Posey Island​", "Blake Island"]
+    campgrounds_too_far_away = ["Camano Island​", "Kitsap Memorial​", "Larrabee​", "South Whidbey​", "Twanoh", "Deception Pass", "Retreat Center - Cornet Bay​", "Potlatch​", "Rasar​", "Fort Ebey​", "Schafer", "Rockport​", "Birch Bay", "Fort Townsend", "Lake Wenatchee​", "Peace Arch", "Sequim Bay​", "Retreat Center - Ramblewood​", "Fort Casey​", "Fort Worden​", "Dosewallups", "Ocean City​", "Retreat Center - Fort Flagler​", "Jarrel Cove", "Pacific Beach​", "Spencer Spit​", "Griffiths-Priday​", "Jones Island​", "Moran​", "Retreat Center - Camp Moran​", "Bogachiel", "Sucia Island​", "Posey Island​", "Blake Island"]
+    filtered_campground_names = []
     visited_section_names = []
     visited_plot_names = []
 
     items = []
-    browser.click_on 'LIST View'
+    select_list_view
     while browser.all(:css, ".resource-name div", text: none_of_these_words_regexp(filtered_campground_names), wait: DEFAULT_WAIT_TIME).present?
       # Click on campground
       campground_selection_url = browser.current_url
       campground_element = browser.all(:css, ".resource-name div", text: none_of_these_words_regexp(filtered_campground_names)).first
       logger.info "Found element with html #{campground_element['innerHTML']}"
       campground_name = campground_element.text
-      break if campground_name.blank?
+      if campground_name.blank?
+        break 
+      end
+      if campgrounds_too_far_away.include? campground_name.strip
+        logger.info "Skipping '#{campground_name}', too far away..."
+        filtered_campground_names << campground_name
+        break 
+      end
       filtered_campground_names << campground_name
       campground_element.click
 
@@ -132,15 +139,15 @@ class Campground < Kimurai::Base
           end
 
           browser.visit plot_selection_url
-          browser.click_on 'LIST View'
+          select_list_view
         end
         visited_plot_names = []
         browser.visit section_selection_url.gsub(/&searchTime=.*$/, '')
-        browser.click_on 'LIST View'
+        select_list_view
       end
       visited_section_names = []
       browser.visit campground_selection_url
-      browser.click_on 'LIST View'
+      select_list_view
     end
     if items.size.positive?
       logger.info "Saved file successfully #{filename}"
@@ -186,6 +193,15 @@ class Campground < Kimurai::Base
       checkbox = browser.find(:css, '.mat-checkbox-label', text: "I have read and acknowledge all of the park alerts listed.")
       checkbox.click
     end
+  end
+
+  def select_list_view()
+    begin
+      list_view_button = browser.all(:css, '.mat-button-toggle-button', text: 'LIST View', wait: 1.5)
+      browser.click_on 'LIST View'
+    rescue Capybara::ElementNotFound => e
+      logger.warn "Unable to find 'List View' button"      
+    end 
   end
 
 end
